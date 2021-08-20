0cc8774424da46a2c3ba73fa04be29fee07914de
Erik Pastor <erik.pastor@glovoapp.com>
2021-08-20 18:21:57 +0200

    Deberes

--- app.js
+++ app.js
@@ -1,41 +1,48 @@
-let hour = 0, minutes = 0, seconds = 0
+let hour = 0,
+  minutes = 0,
+  seconds = 0;
 
-let isStarted = false
+let isStarted = false;
+
+let timerIntervalId;
 
 const toggleTimer = () => {
-    isStarted = !isStarted
-    return isStarted
-}
+  isStarted = !isStarted;
+  return isStarted;
+};
 
 const calculateTime = () => {
-    if (seconds < 60) {
-        seconds++
-    } else if (seconds === 60) {
-        minutes++
-        seconds = 0
-    }
+  if (seconds < 60) {
+    seconds++;
+  } else if (seconds === 60) {
+    minutes++;
+    seconds = 0;
+  }
 
-    if (minutes === 60) {
-        hour++
-        minutes = 0
-    }
+  if (minutes === 60) {
+    hour++;
+    minutes = 0;
+  }
 
-    document.getElementById("display").innerHTML = `${hour < 10 ? '0' + hour : hour}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`
-}
+  // Guardar el elemento display en una variable y reusarla en vez de llamar a document.getElementById cada vez
+  // hacer que la linea 29 a 33 sea una función aparte para poder reutilizarla en varias funciones Pista: Te servirá para el botón clear
+  document.getElementById("display").innerHTML = `${
+    hour < 10 ? "0" + hour : hour
+  }:${minutes < 10 ? "0" + minutes : minutes}:${
+    seconds < 10 ? "0" + seconds : seconds
+  }`;
+};
 
-const incrementTimer = setInterval(calculateTime, 1000)
-clearInterval(incrementTimer)
-
-const startStopButton = document.getElementById("startStop")
-startStopButton.addEventListener('click', () => {
-    const buttonState = toggleTimer()
-    if (buttonState) {
-        startStopButton.innerHTML = "Stop";
-        incrementTimer()
-    } else {
-        startStopButton.innerHTML = "Start";
-        clearInterval(incrementTimer)
-    }
+const startStopButton = document.getElementById("startStop");
+startStopButton.addEventListener("click", () => {
+  const buttonState = toggleTimer();
+  if (buttonState) {
+    startStopButton.innerHTML = "Stop";
+    timerIntervalId = setInterval(calculateTime, 1000);
+  } else {
+    startStopButton.innerHTML = "Start";
+    clearInterval(timerIntervalId);
+  }
 });
 
-/*  */ 
+// Hacer que el botón reset pare el contador si esta iniciado y reinicie todo a 0
